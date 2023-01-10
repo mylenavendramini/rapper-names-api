9d7236b20221b67b1dde8ad4ab5496e8c88858d4
mylenavendramini <myvendramini@gmail.com>
2023-01-10 17:56:31 +0000

    deploy render

--- public/js/index.js
+++ public/js/index.js
@@ -5,7 +5,7 @@ async function apiRequest() {
   const rapperName = document.querySelector("input").value;
   try {
     const response = await fetch(
-      `https://https://rap-names-api-0y3b.onrender.com/api/${rapperName}`
+      `https://rap-names-api-0y3b.onrender.com/api/${rapperName}`
     );
 
     const data = await response.json();
