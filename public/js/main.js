50e2cb982d3d829f39e4d410183e85e252c2d00b
mylenavendramini <myvendramini@gmail.com>
2023-01-12 09:33:33 +0000

    put and delete

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,70 @@
-document.querySelector("button").addEventListener("click", apiRequest);
+// document.querySelector("button").addEventListener("click", apiRequest);
 
-async function apiRequest() {
-  const rapperName = document.querySelector("input").value;
+// async function apiRequest() {
+//   const rapperName = document.querySelector("input").value;
+//   try {
+//     const response = await fetch(
+//       `https://rap-names-api-0y3b.onrender.com/api/${rapperName}`
+//     );
+
+//     const data = await response.json();
+
+//     console.log(data);
+//     document.querySelector("h2").innerText = data.birthName;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+const deleteText = document.querySelectorAll(".fa-trash");
+const thumbText = document.querySelectorAll(".fa-thumbs-up");
+
+Array.from(deleteText).forEach((element) => {
+  element.addEventListener("click", deleteRapper);
+});
+
+Array.from(thumbText).forEach((element) => {
+  element.addEventListener("click", addLike);
+});
+
+async function deleteRapper() {
+  const sName = this.parentNode.childNodes[1].innerText;
+  const bName = this.parentNode.childNodes[3].innerText;
   try {
-    const response = await fetch(
-      `https://rapper-names-api.netlify.app/api/${rapperName}`
-    );
-
+    const response = await fetch("deleteRapper", {
+      method: "delete",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        stageNameS: sName,
+        birthNameS: bName,
+      }),
+    });
     const data = await response.json();
-
     console.log(data);
-    document.querySelector("h2").innerText = data.birthName;
-  } catch (error) {
-    console.log(error);
+    location.reload();
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function addLike() {
+  const sName = this.parentNode.childNodes[1].innerText;
+  const bName = this.parentNode.childNodes[3].innerText;
+  const tLikes = Number(this.parentNode.childNodes[5].innerText);
+  try {
+    const response = await fetch("addOneLike", {
+      method: "put",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        stageNameS: sName,
+        birthNameS: bName,
+        likesS: tLikes,
+      }),
+    });
+    const data = await response.json();
+    console.log(data);
+    location.reload();
+  } catch (err) {
+    console.log(err);
   }
 }
--- server.js
+++ server.js
@@ -1,48 +1,83 @@
 const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const PORT = 8000;
-
 const app = express();
-// router.use(bodyParser.json());
-app.use(bodyParser.json());
-app.use(cors());
-// app.use("/static", express.static("./static/"));
+const cors = require("cors");
+const { response } = require("express");
+const PORT = 8000;
+const MongoClient = require("mongodb").MongoClient;
+require("dotenv").config();
 
-const rappers = {
-  "21 savage": {
-    age: 29,
-    birthName: "Shéyaa Bin Abraham-Joseph",
-    birtLocation: "London, England",
-  },
-  "chance the rapper": {
-    age: 29,
-    birthName: "Chancelor Johnathan Bennett",
-    birtLocation: "Chicago, Illinois",
-  },
-  dylan: {
-    age: 32,
-    birthName: "Dylan Joel Smith",
-    birtLocation: "Madrid, Spain",
-  },
-};
+let db,
+  dbConnectionStr = process.env.DB_STRING,
+  dbName = "rappers-name";
+
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }).then(
+  (client) => {
+    console.log(`Connected to ${dbName} Database`);
+    db = client.db(dbName);
+  }
+);
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use(express.static("public"));
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+
+app.get("/", (request, response) => {
+  db.collection("rappers")
+    .find()
+    .sort({ likes: -1 })
+    .toArray()
+    .then((data) => {
+      response.render("index.ejs", { info: data });
+    })
+    .catch((error) => console.error(error));
 });
 
-app.get("/api", (req, res) => {
+// app.get("/api", (req, res) => {
+//   res.json(rappers);
+// });
+
+// app.get("/api/:name", (req, res) => {
+//   const rappersName = req.params.name.toLowerCase();
+//   if (rappers[rappersName]) {
+//     res.json(rappers[rappersName]);
+//   } else {
+//     res.json(rappers["unknown"]);
+//   }
+// });
+
+app.post("/addRapper", (req, res) => {
+  db.collection("rappers")
+    .insertOne({
+      stageName: req.body.stageName,
+      birthName: req.body.birthName,
+      likes: 0,
+    })
+    .then((result) => {
+      console.log("Rapper Added");
+      res.redirect("/");
+    })
+    .catch((error) => console.error(error));
+});
+
+app.delete("/deleteRapper", (request, response) => {
+  db.collection("rappers")
+    .deleteOne({ stageName: request.body.stageNameS })
+    .then((result) => {
+      console.log("Rapper Deleted");
+      response.json("Rapper Deleted");
+    })
+    .catch((error) => console.error(error));
+});
+
+app.put("/addOneLike", (req, res) => {
+  db.collection("rappers").updateOne({ stageName: req.body.stageName });
+});
+
+app.get("/rappers", (req, res) => {
   res.json(rappers);
-});
-
-app.get("/api/:name", (req, res) => {
-  const rappersName = req.params.name.toLowerCase();
-  if (rappers[rappersName]) {
-    res.json(rappers[rappersName]);
-  } else {
-    res.json(rappers["unknown"]);
-  }
 });
 
 app.listen(process.env.PORT || PORT, () => {
